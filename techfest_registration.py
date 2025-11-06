c64a6c5864bbe0af06074b861c1c9796a9c98b82
Sabella <12413776@benilde.edu.ph>
2025-11-06 12:50:41 +0800

    Implemented the Registered Participants in registration

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,4 +1,5 @@
 # Tech Fest Midterm Exam
+# TASK 1
 print("Welcome to SMIT TechFest!")
 print("Event organized by Khirk of APPDAET BTCS2")
 print("\n")
@@ -7,3 +8,16 @@ parti = int(input("How many participants will register? "))
 if parti <= 0:
     print("Invalid number of participants.")
     exit()
+
+# TASK 2
+participants = []
+for i in range(parti):
+    name = (input(f"Enter participant #{i+1} name: "))
+    track = (input("Enter chosen track: "))
+    participants.append({"name": name,  "track": track})
+
+print("\n")
+print("Registered Participants:")
+for i in range(len(participants)):
+    print(f"{i + 1}. {participants[i]['name']} - {participants[i]['track']}")
+
