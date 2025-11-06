3d22615ffeeb7377ba054e59d5f51094dc086f98
Sabella <12413776@benilde.edu.ph>
2025-11-06 12:09:42 +0800

    Implemented number of participant registration

---
+++ techfest_registration.py
@@ -0,0 +1,9 @@
+# Tech Fest Midterm Exam
+print("Welcome to SMIT TechFest!")
+print("Event organized by Khirk of APPDAET BTCS2")
+print("\n")
+
+parti = int(input("How many participants will register? "))
+if parti <= 0:
+    print("Invalid number of participants.")
+    exit()
